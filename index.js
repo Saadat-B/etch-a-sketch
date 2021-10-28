6f9461eedfcb5fd0483eb510d8e0d41a56eb3585
Saadat Badgujar <saadatbadgujar@gmail.com>
2021-10-28 09:07:44 +0530

    Added Color and Clear Functionality

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const body = document.querySelector('body');
+const clearBtn = document.querySelector('#clear');
 
 const container = document.createElement('div');
 container.className = 'container';
 body.appendChild(container);
+const grids = container.childNodes;
+let numOfGrids;
+let grid;
+
+
 
 for(let i = 0;i < 16*16; i++ ){
-    const grid = document.createElement('div');
+    grid = document.createElement('div');
     grid.className = 'grid';
     container.appendChild(grid);
 };
-console.log(container.childElementCount);
+
+
+function fill(){
+    this.classList.add('fill-in');
+}
+function clear(){
+    grids.forEach(grid => grid.classList.remove('fill-in'))
+}
+
+grids.forEach(grid => grid.addEventListener('mouseenter',fill));
+clearBtn.addEventListener('click', clear);
+
+
 
 
 
